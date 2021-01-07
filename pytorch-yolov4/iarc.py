7215bdd757ab650919858e393d77c65c6beb43a6
fun-math <mallikakumar1729@gmail.com>
2021-01-07 11:09:04 +0530

    repo+iarc.py

---
+++ pytorch-yolov4/iarc.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np 
+
+from tool.darknet2pytorch import Darknet
+from demo import *
+
+board=Darknet('cfg/yolov4_box.cfg',inference=True)
+board.load_weights('backup/yolov4_box_4000.weights')
+board.cuda()
+
+module=Darknet('cfg/yolov4_module.cfg',inference=True)
+module.load_weights('backup/yolov4_module_4000.weights')
+module.cuda()
+
+def my_detect(m,cv_img):
+  use_cuda=True
+  img=cv2.resize(cv_img, (m.width, m.height))
+  img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  boxes = do_detect(m, img, 0.4, 0.6, use_cuda)
+  if len(boxes)==0:
+  	return 0
+  box=boxes[0][0]
+  h,w,c=cv_img.shape
+  x1 = int(box[0] * w)
+  y1 = int(box[1] * h)
+  x2 = int(box[2] * w)
+  y2 = int(box[3] * h)
+  return [x1,y1,x2,y2]
+  
+def end_to_end(board,module,cv_img):
+  use_cuda=True
+  img=cv2.resize(cv_img, (board.width, board.height))
+  img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  boxes = do_detect(board, img, 0.4, 0.6, use_cuda)
+  if len(boxes)==0:
+  	return 0
+  box=boxes[0][0]
+  h,w,c=cv_img.shape
+  x1 = int(box[0] * w)
+  y1 = int(box[1] * h)
+  x2 = int(box[2] * w)
+  y2 = int(box[3] * h)
+  
+  hc=y2-y1
+  wc=x2-x1
+
+  cropped=cv2.resize(cv_img[y1:y2,x1:x2], (module.width,module.height))
+  imShow(cropped)
+  cropped=cv2.cvtColor(cropped, cv2.COLOR_BGR2RGB)
+  boxes=do_detect(module, cropped, 0.4, 0.6, use_cuda)
+  box=boxes[0][0]
+  print(box,hc,wc,x1,y1)
+  a1=x1+int(box[0]*wc)
+  b1=y1+int(box[1]*hc)
+  a2=x1+int(box[2]*wc)
+  b2=y1+int(box[3]*hc)
+  print(a1,b1,a2,b2)
+  return [a1,b1,a2,b2]
+
